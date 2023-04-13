077c36810b4da6d68c99d32679e71b455793de0f
Carl_Javier <ccjavier87@gmail.com>
2023-04-12 22:18:54 -0700

    complete lab

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,140 +1,196 @@
 
-const gameObject = {
-    home: {
-        teamName: 'Brooklyn Nets',
-        colors: ['Black', 'White'],
-        players: {
+function gameObject() {
+    return {
+        home: {
+            "teamName": 'Brooklyn Nets',
+            "colors": ['Black', 'White'],
+            "players": {
 
-            "Alan Anderson": {
-                number: "16",
-                shoe: "16",
-                points: "22",
-                rebounds: "12",
-                assists: "12",
-                steals: "3",
-                blocks: "1",
-                slamDunks: "1"
+                "Alan Anderson": {
+                    "number": 16,
+                    "shoe": 16,
+                    "points": 22,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 1
 
-            },
-
-
-            "Reggie Evans": {
-                number: "30",
-                shoe: "14",
-                points: "12",
-                rebounds: "12",
-                assists: "12",
-                steals: "12",
-                blocks: "12",
-                slamDunks: "7"
-            },
+                },
 
 
-            "Brook Lopez": {
-                number: "11",
-                shoe: "17",
-                points: "17",
-                rebounds: "19",
-                assists: "10",
-                steals: "3",
-                blocks: "1",
-                slamDunks: "15"
-            },
+                "Reggie Evans": {
+                    "number": 30,
+                    "shoe": 14,
+                    "points": 12,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 12,
+                    "blocks": 12,
+                    "slamDunks": 7
+                },
 
 
-            "Mason Plumlee": {
-                number: "1",
-                shoe: "17",
-                points: "17",
-                rebounds: "19",
-                assists: "10",
-                steals: "3",
-                blocks: "8",
-                slamDunks: "5"
-            },
+                "Brook Lopez": {
+                    "number": 11,
+                    "shoe": 17,
+                    "points": 17,
+                    "rebounds": 19,
+                    "assists": 10,
+                    "steals": 3,
+                    "blocks": 1,
+                    "slamDunks": 15
+                },
 
 
-            "Jason Terry": {
-                number: "31",
-                shoe: "15",
-                points: "19",
-                rebounds: "2",
-                assists: "2",
-                steals: "4",
-                blocks: "11",
-                slamDunks: "1"
+                "Mason Plumlee": {
+                    "number": 1,
+                    "shoe": 17,
+                    "points": 17,
+                    "rebounds": 19,
+                    "assists": 10,
+                    "steals": 3,
+                    "blocks": 8,
+                    "slamDunks": 5
+                },
+
+
+                "Jason Terry": {
+                    "number": 31,
+                    "shoe": 15,
+                    "points": 19,
+                    "rebounds": 2,
+                    "assists": 2,
+                    "steals": 4,
+                    "blocks": 11,
+                    "slamDunks": 1
+                }
+
             }
+        },
 
+        away: {
+            "teamName": "Charlotte Hornets",
+            "colors": ["Turquoise", " Purple"],
+            "players": {
+
+                "Jeff Adrien": {
+                    "number": 4,
+                    "shoe": 18,
+                    "points": 10,
+                    "rebounds": 1,
+                    "assists": 1,
+                    "steals": 2,
+                    "blocks": 7,
+                    "slamDunks": 2
+                },
+
+
+                "Bismak Biyombo": {
+
+                    "number": 0,
+                    "shoe": 16,
+                    "points": 12,
+                    "rebounds": 4,
+                    "assists": 7,
+                    "steals": 7,
+                    "blocks": 15,
+                    "slamDunks": 10
+
+                },
+
+                "DeSagna Diop": {
+
+                    "number": 2,
+                    "shoe": 14,
+                    "points": 24,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 4,
+                    "blocks": 5,
+                    "slamDunks": 5
+                },
+
+
+                "Ben Gordon": {
+                    "number": 8,
+                    "shoe": 15,
+                    "points": 33,
+                    "rebounds": 3,
+                    "assists": 2,
+                    "steals": 1,
+                    "blocks": 1,
+                    "slamDunks": 0
+                },
+
+
+                "Brendan Haywood": {
+                    "number": 33,
+                    "shoe": 15,
+                    "points": 6,
+                    "rebounds": 12,
+                    "assists": 12,
+                    "steals": 22,
+                    "blocks": 5,
+                    "slamDunks": 12
+                }
+
+            }
         }
-    },
+    }
 
-    away: {
-        teamName: "Charlotte Hornets",
-        colors: ["Turquoise", " Purple"],
-        players: {
-
-            "Jeff Adrien": {
-                number: "4",
-                shoe: "18",
-                points: "10",
-                rebounds: "1",
-                assists: "1",
-                steals: "2",
-                blocks: "7",
-                slamDunks: "2"
-            },
+}
 
 
-            "Bismak Biyombo": {
+function homeTeamName () {
+    let teamInfo = gameObject();
+    return teamInfo ["home"]["teamName"];
+}
 
-                number: "0",
-                shoe: "16",
-                points: "12",
-                rebounds: "4",
-                assists: "7",
-                steals: "7",
-                blocks: "15",
-                slamDunks: "10"
-
-            },
-
-            "DeSagna Diop": {
-
-                number: "2",
-                shoe: "14",
-                points: "24",
-                rebounds: "12",
-                assists: "12",
-                steals: "4",
-                blocks: "5",
-                slamDunks: "5"
-            },
+function awayTeamName () {
+    let teamInfo = gameObject();
+    return teamInfo ["away"]["teamName"];
+}
 
 
-            "Ben Gordon": {
-                number: "8",
-                shoe: "15",
-                points: "33",
-                rebounds: "3",
-                assists: "2",
-                steals: "1",
-                blocks: "1",
-                slamDunks: "0"
-            },
-
-
-            "Brendan Haywood": {
-                number: "33",
-                shoe: "15",
-                points: "6",
-                rebounds: "12",
-                assists: "12",
-                steals: "22",
-                blocks: "5",
-                slamDunks: "12"
+function numPointsScored(name) {
+    let obj = gameObject();
+    let objKeys = Object.keys(obj)
+    for (let k of objKeys) {
+        let players = Object.keys(obj[k]["players"])
+        for (let p of players) {
+            if (p === name) {
+                return obj[k]["players"][p]["points"]
             }
+        }
 
+    }
+
+}
+
+function whichPlayer(name) {
+    let obj = gameObject()
+    for (let team in obj) {
+        let teamPlayers = obj[team].players
+        for (let player in teamPlayers) {
+            if (player === name) {
+                return teamPlayers[name]
+            }
         }
     }
 }
 
+function shoeSize(name) {
+    let thisOne = whichPlayer(name)
+    if (thisOne) return thisOne["shoe"]
+}
+
+
+
+function teamColors () {}
+
+function teamNames () {}
+
+function playerNumbers () {}
+
+function playerStats () {}
